4b8a66ce073fed6c132ebb2e1b2b249ef07a58f1
samirayounesy <47450280+samirayounesy@users.noreply.github.com>
2019-03-21 12:19:46 +0100

    Constructeur item

--- js/personnage.js
+++ js/personnage.js
@@ -199,10 +199,10 @@ class personnage{
 		//déplacer le personnage selon l'évènement déclenché 
 
 		//vérifier si un obstacle se trouve sur la trajectoire 
-		//if (this.detecter_obstacle(x)){
+		if (this.detecter_obstacle(x)){
 
-		//	return 
-		//}
+			return 
+		}
 
 
 		//sauvegarde des coordonnées actuelles pour supprimer
@@ -266,6 +266,8 @@ class personnage{
 		this.vitesse = x
 
 	}
+
+
 
 
 
