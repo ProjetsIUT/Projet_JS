a625c89c97af66a7f257e3ef2585ae48bf2fe091
Yael Misseri <yael.misseri@etu.umontpellier.fr>
2019-03-21 10:34:08 +0100

    Images

--- js/carte.js
+++ js/carte.js
@@ -1,12 +1,44 @@
 class carte {
 
+
+
 	constructor(canvas){
 
 		this.canvas = canvas //le canvas où se déroule jeu 
 		this.context = this.canvas.getContext('2d') //contexte du canvas
 		this.canvas.crossOrigin = "Anonymous"
+		this.items = {}
 
 	}
+
+	add_item(item){
+
+		this.items.push(item);
+
+	}
+
+	remove_item(item){
+
+		//suprimer un item de la table des items
+
+		for(let i=0; i<items.length; i++){
+
+			if(this.items[i]==item){
+
+				this.items.splice(i,1)
+			}
+		}
+
+	}
+
+	item_here(x,y){
+
+		//vérifier si un item 
+
+	}
+
+
+
 
 	set_background(){
 
