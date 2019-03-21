a30ed7d81c04965dee3940e5ab5720201347edce
samirayounesy <47450280+samirayounesy@users.noreply.github.com>
2019-03-21 12:45:03 +0100

    Update item.js

--- js/item.js
+++ js/item.js
@@ -12,8 +12,56 @@ class Item {
 		this.largeur = 20 //Largeur de l'image en px
 		this.hauteur = 20 //hauteur de l'image en px
 		this.itemtab={'chest','heart','key','shield'}	
+		this.tabSalle=new Array(32)
+		for(int i=0 ; i<32; i++){
+			tabSalle[i]=new Array(2)
+		}
+
+		
 
 	}
+
+	choixItemPosition(i){
+
+		if (this.itemtab[i]=='chest'){
+			var x=Mathrandom()*(20-1)+1
+				if(x==19 || x==20 || x==1 || x==3 ){
+					this.posY=534
+					if(x==19){
+						this.posX=90
+					} else{
+						if(x==20){
+							this.posX=222
+						} else {
+							if(x==1){
+								this.posX=330
+							} else {
+								if(x==2){
+									this.posX=442
+								} else{
+									if(x==3){
+										this.posX=542
+									}
+								}
+							}
+						}
+					}
+				}
+		}
+
+
+
+
+
+
+
+
+
+
+	}
+
+
+
 
 
 	
