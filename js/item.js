d72c57d13547092961633786b1c0a5eeba6656dc
samirayounesy <47450280+samirayounesy@users.noreply.github.com>
2019-03-21 10:48:36 +0100

    Update item.js

---
+++ js/item.js
@@ -0,0 +1,44 @@
+class Item {
+
+	constructor(name, icone, canvas){
+
+		this.name = name //nom de l'item
+		this.canvas = canvas  //canvas du jeu
+		this.context = this.canvas.getContext('2d') //contexte du canvas
+		this.image = new Image() //image de l'item
+		this.image.src = icone //attribuer la source de l'icone à notre image
+		
+		this.posX = Math.random() * (this.canvas.width - 0) + 0//position X de l'image
+		this.posY = Math.random() * (this.canvas.height - 0) + 0 //position Y de l'image
+		this.largeur = 20 //Largeur de l'image en px
+		this.hauteur = 20 //hauteur de l'image en px
+		
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
