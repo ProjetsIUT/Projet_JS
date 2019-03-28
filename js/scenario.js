97bede73f3082d715e9014206043388e6e439264
samirayounesy <47450280+samirayounesy@users.noreply.github.com>
2019-03-29 00:32:06 +0100

    Update scenario.js

--- js/scenario.js
+++ js/scenario.js
@@ -151,7 +151,7 @@ function beginning() {
   canvas = document.getElementById("carte")
   C = new carte(canvas)
   P = new player("Joueur", C,290,460)
-  P2 = new player("Joueur", C, 290,490)
+  P2 = new player2("Joueur", C, 290,490)
   tab_ennemis = [new ennemi("Ennemi1",C,20,20), new ennemi("Ennemi2",C,100,100), new ennemi("Ennemi3",C,200,200),new ennemi("Ennemi4",C,330,480),
   new ennemi("Ennemi5",C,330,480), new ennemi("Ennemi6",C,197,468), new ennemi("Ennemi7",C,547,239), new ennemi("Ennemi8",C,319,128),
   new ennemi("Ennemi9",C,456,466), new ennemi("Ennemi10",C,662,295)]
